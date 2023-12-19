14d964176311c9bf1365f021702bcba740d28fd3
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2023-12-19 11:45:25 +0100

    Finished

--- src/ros2_course/ros2_course/hello.py
+++ src/ros2_course/ros2_course/hello.py
@@ -1,57 +1,126 @@
-#!/usr/bin/env python3
-
 import rclpy
+
 from rclpy.node import Node
+
 from geometry_msgs.msg import Twist
+
 import math
+
 import time
 
-class KochSnowflake(Node):
+
+
+class Snowflake(Node):
+
+
 
     def __init__(self):
-        super().__init__('koch_snowflake')
-        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.length = 4.0  # A vonal kezdeti hossza
-        self.angle = 60.0  # A fordulási szög fokban
 
-    def draw_koch_segment(self, length, level):
-        if level == 0:
-            move_cmd = Twist()
-            move_cmd.linear.x = length
-            self.publisher_.publish(move_cmd)
-            time.sleep(1)  # Adjon időt a turtlesimnek a mozgásra
-        else:
-            length /= 3.0
-            self.draw_koch_segment(length, level - 1)
-            self.rotate_turtle(-self.angle)
-            self.draw_koch_segment(length, level - 1)
-            self.rotate_turtle(2 * self.angle)
-            self.draw_koch_segment(length, level - 1)
-            self.rotate_turtle(-self.angle)
-            self.draw_koch_segment(length, level - 1)
+        super().__init__('snowflake')
+
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        self.length = 4.0
+
+        self.angle = 45.0
+
+
+
+    def move_turtle_forward(self, length):
+
+        move_cmd = Twist()
+
+        move_cmd.linear.x = length
+
+        self.publisher_.publish(move_cmd)
+
+        time.sleep(1)
+
+
 
     def rotate_turtle(self, angle):
-        turn_cmd = Twist()
-        turn_cmd.angular.z = math.radians(angle)
-        self.publisher_.publish(turn_cmd)
-        time.sleep(1)  # Adjon időt a turtlesimnek a fordulásra
 
-    def draw_snowflake(self):
-        # Először elforgatjuk a teknőst, hogy a kezdővonal a hópehely egyik csúcsától induljon
-        self.rotate_turtle(-90)  # Elforgatás felfelé
-        # A hópehely minden oldalának megrajzolása
-        for _ in range(3):
-            self.draw_koch_segment(self.length, 3)  # A rekurzió mélységét itt állítjuk be
+        turn_cmd = Twist()
+
+        turn_cmd.angular.z = math.radians(angle)
+
+        self.publisher_.publish(turn_cmd)
+
+        time.sleep(1)
+
+
+
+    def draw_segment(self, length):
+
+        self.move_turtle_forward(length)
+
+
+
+    def draw_side_of_snowflake(self, iterations):
+
+        segment_length = self.length / (3 ** iterations)
+
+        for _ in range(iterations):
+
+            self.draw_segment(segment_length)
+
+            self.rotate_turtle(-self.angle)
+
+            self.draw_segment(segment_length)
+
+            self.rotate_turtle(2 * self.angle)
+
+            self.draw_segment(segment_length)
+
+            self.rotate_turtle(-self.angle)
+
+            self.draw_segment(segment_length)
+
+
+
+    def draw_full_snowflake(self, iterations):
+
+        self.rotate_turtle(-90)  
+
+        for _ in range(5):
+
+            self.draw_side_of_snowflake(iterations)
+
             self.rotate_turtle(120)
 
+
+
+    def stop_turtle(self):
+
+        self.publisher_.publish(Twist())
+
+
+
 def main(args=None):
+
     rclpy.init(args=args)
-    koch_snowflake = KochSnowflake()
-    koch_snowflake.draw_snowflake()
-    koch_snowflake.publisher_.publish(Twist())  # Megállítjuk a teknőst
-    koch_snowflake.destroy_node()
+
+    s = Snowflake()
+
+    
+
+    # Set the number of iterations as needed (e.g., 2 in this example)
+
+    iterations = 3
+
+    s.draw_full_snowflake(iterations)
+
+    
+
+    s.stop_turtle()
+
+    s.destroy_node()
+
     rclpy.shutdown()
 
+
+
 if __name__ == '__main__':
+
     main()
 
